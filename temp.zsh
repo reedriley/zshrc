d0f17644ceb2a1da8ec3dd0765432ccf562c70c8
John Reed Riley <john.reed.riley@gmail.com>
2011-04-02 13:25:56 -0700

    Added warning message when tmpreaper is not installed.

--- temp.zsh
+++ temp.zsh
@@ -5,6 +5,8 @@ TMPLOC=/tmp/.${USER}.tmp/
 mkdir -p ${TMPLOC}
 if [[ -x `which tmpreaper` ]]; then
 	`which tmpreaper` -s 1d ${TMPLOC}
+else
+  echo "WARNING: tmpreaper not installed."
 fi
 ln -fns ${TMPLOC} ${TMPDIR}
 
