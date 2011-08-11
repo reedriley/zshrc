bd8510c0f479c9a5ae98e2f80229cfdd7d1371c2
Reed Riley <john.reed.riley@gmail.com>
2011-08-11 01:12:46 -0700

    Added Homebrew's ccache location to the PATH.

--- ccache.zsh
+++ ccache.zsh
@@ -4,5 +4,7 @@ if [ -d /usr/lib/ccache/bin ]; then
 	export PATH="/usr/lib/ccache/bin:${PATH}"
 elif [ -d /usr/lib/ccache ]; then
 	export PATH="/usr/lib/ccache:${PATH}"
+elif [ -d /usr/local/Cellar/ccache/3.1.5/libexec ]; then
+  export PATH="/usr/local/Cellar/ccache/3.1.5/libexec:${PATH}"
 fi
 
